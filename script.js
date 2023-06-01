7bcf8103529d1fe6572e432e4e9db7afdfc3c7b3
Tejaswini Hirudkar <121488475+TejaswiniHirudkar@users.noreply.github.com>
2023-06-01 20:07:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 // complete the given function
 
 function palindrome(str){
-
+	let strwithoutspace=str.replace(/\s/g, "");
+	let i=0;
+	let j=strwithoutspace.length-1;
+	while(i<j){
+		if(strwithoutspace.charAt(i)==strwithoutspace.charAt(j)){
+			i++;
+			j--;
+		}
+		else{
+			return false;
+		}
+	}
+	return true;
 }
 module.exports = palindrome
